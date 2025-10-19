287fd9009839659e4e122dbc3993643cc6d81f8b
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-10-19 00:18:56 -0400

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,12 +1,15 @@
+'use client';
 
-export default function navBar(){
+export default function navBar({ onSignupClick, onLoginClick }){
     return <div className="flex justify-between items-center p-4">
         <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600
-        transition-all duration-300 ease-in-out cursor-pointer">
+        transition-all duration-300 ease-in-out cursor-pointer"
+        onClick={onSignupClick}>
             Sign Up
         </button>
         <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 
-        transition-all duration-300 ease-in-out cursor-pointer">
+        transition-all duration-300 ease-in-out cursor-pointer"
+        onClick={onLoginClick}>
             Login
         </button>
     </div>
