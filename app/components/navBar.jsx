96aa4cd40f6f300b1430bcc7069d671ddb84392b
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-10-19 15:56:58 -0400

    Add files via upload

---
+++ app/components/navBar.jsx
@@ -0,0 +1,19 @@
+"use client"
+
+export default function NavBar(){
+    return <div className="flex justify-center items-center p-4 bg-slate-950 border-b border-slate-700 shadow-xl">
+        <div className="text-left text-2xl font-bold text-blue-500 text-center">
+           TradingView
+        </div>
+        <div className="flex justify-end items-center gap-4 ml-auto">
+        <button className="bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600 transition-all duration-300 cursor-pointer
+        font-medium text-sm text-center">
+            Sign Up
+        </button>
+        <button className="bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600 transition-all duration-300 cursor-pointer
+        font-medium text-sm text-center">
+            Login
+        </button>
+        </div>
+    </div>
+}
